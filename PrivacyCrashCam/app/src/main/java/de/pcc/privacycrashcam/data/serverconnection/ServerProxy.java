d2b8aaa6fc13eadf027138c01aa72132800ade8b
Giorgio Groß <uvech@student.kit.edu>
2017-02-26 00:37:44 +0100

    Check if user is logged in before uploading files

--- PrivacyCrashCam/app/src/main/java/de/pcc/privacycrashcam/data/serverconnection/ServerProxy.java
+++ PrivacyCrashCam/app/src/main/java/de/pcc/privacycrashcam/data/serverconnection/ServerProxy.java
@@ -15,15 +15,53 @@ import de.pcc.privacycrashcam.data.Account;
 public class ServerProxy {
     private Context context;
 
-    public ServerProxy (Context context) {
+    public ServerProxy(Context context) {
         this.context = context;
     }
 
-    public void videoUpload(File videoFile, File metadata, File symKey, Account account,
-                            ServerResponseCallback<RequestState> callback) {
-        new VideoUploadTask(videoFile, metadata, symKey, account, callback, context).execute(ServerHelper.URL);
+    /**
+     * Uploads the passed video along with the passed metadata and key to the account
+     *
+     * @param videoFile Video to be uploaded
+     * @param metadata  Metadata of the video
+     * @param symKey    Key of the video and metadata
+     * @param account   Account to link the files with
+     * @param callback  Observer which will be notified about state changes of the upload
+     */
+    public void videoUpload(final File videoFile, final File metadata,
+                            final File symKey, final Account account,
+                            final ServerResponseCallback<RequestState> callback) {
+        // check if password and mail are still valid
+        authenticateUser(account, new ServerResponseCallback<AuthenticationState>() {
+            @Override
+            public void onResponse(AuthenticationState response) {
+                if (response == AuthenticationState.SUCCESS) {
+                    // we are logged in - proceed with file upload
+                    new VideoUploadTask(videoFile, metadata, symKey, account, callback, context)
+                            .execute(ServerHelper.URL);
+                } else {
+                    callback.onResponse(RequestState.ACCOUNT_FAILURE);
+                }
+            }
+
+            @Override
+            public void onProgress(int percent) {
+                // ignored
+            }
+
+            @Override
+            public void onError(String error) {
+                callback.onError(error);
+            }
+        });
     }
 
+    /**
+     * Authenticate the user. This will check if the password and mail match.
+     *
+     * @param account  Account which needs to be authenticated.
+     * @param callback Observer which will be notified about state changes of the authentication
+     */
     public void authenticateUser(Account account,
                                  ServerResponseCallback<AuthenticationState> callback) {
         new AuthenticateTask(account, callback, context).execute(ServerHelper.URL);
