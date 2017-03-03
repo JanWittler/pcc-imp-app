8b75d4e82d9f9310fe82b7e6004fe1aa483c8392
Giorgio Groß <uvech@student.kit.edu>
2017-03-03 20:34:17 +0100

    Rmove utility method from concrete test class

--- PrivacyCrashCam/app/src/androidTest/java/de/pcc/privacycrashcam/data/memoryaccess/MemoryManagerTest.java
+++ PrivacyCrashCam/app/src/androidTest/java/de/pcc/privacycrashcam/data/memoryaccess/MemoryManagerTest.java
@@ -1,20 +1,9 @@
 package de.pcc.privacycrashcam.data.memoryaccess;
 
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.Assert.*;
-
 /**
  * @author Giorgio Gross
  */
 public class MemoryManagerTest {
 
-    @SuppressWarnings("ResultOfMethodCallIgnored")
-    public static File CreateFile (File dir, String name) throws IOException {
-        File file = new File(dir, name);
-        file.createNewFile();
-        return file;
-    }
 
 }
